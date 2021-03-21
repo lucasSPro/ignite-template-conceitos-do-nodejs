1f00cfb0d0751cd89e5e990388acad65726e2dee
Lucas dos Santos <31446385+lucasSPro@users.noreply.github.com>
2021-03-21 11:41:04 -0300

    feature: change the validation in the middleware, id to username params

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const{username} = request.headers;
+
+  const user =  users.find(item => item.username === username);
+
+  if(!user){
+    return response.status(400).json({error: "user already exists"});
+  };
+
+  request.user = user;
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users',  (request, response) => {
+  const {name, username} = request.body;
+
+  const usernameAlreadyExists =  users.find(user => user.username === username);
+
+  if(usernameAlreadyExists){
+    return response.status(400).json({error: "username already exists"});
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+  users.push(user);
+
+  return response.status(201).json(user);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
